00e3ca83c9db7563d6235d3aff3e83253a1eb945
Qiankang Mao <qiankangmao@QiankangdeMacBook-Pro.local>
2019-04-27 21:21:24 +0800

    first commit

---
+++ app_my_blog/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AppMyBlogConfig(AppConfig):
+    name = 'app_my_blog'
---
+++ my_blog/urls.py
@@ -0,0 +1,23 @@
+"""my_blog URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.conf.urls import url
+from django.contrib import admin
+from app_my_blog import views as app_my_blog_views  # new
+
+urlpatterns = [
+    url(r'^$', app_my_blog_views.index),  # new
+    url(r'^admin/', admin.site.urls),
+]
